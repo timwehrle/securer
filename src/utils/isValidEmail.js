e7161499570c33426e68aa6833bc3145ce0c1233
Tim <115550741+weti09@users.noreply.github.com>
2023-12-09 13:43:28 +0100

    feat: new updated validation techniques

--- src/utils/isValidEmail.js
+++ src/utils/isValidEmail.js
@@ -5,38 +5,60 @@ const DEFAULT_OPTIONS = {
     requireTld: true,
     allowUnderscores: true,
     allowPlusSign: true,
+    allowComments: true,
+    allowQuotations: true,
     validateLength: true
 }
 
 export default function isValidEmail(email, customOptions) {
-    if (!isString(email)) {
+    if (!isString(email) || !email.includes('@')) {
         return false;
-    };
+    }
 
     isOption(customOptions, DEFAULT_OPTIONS);
     const options = { ...DEFAULT_OPTIONS, ...customOptions};
 
-    try {
-        if(!email.contains('@')) {
-            return false;
-        }
-       
-        const [localePart, domain] = email.split('@');
+    email = email.replace(/\s/g, '');
 
-        if (options.validateLength && localePart.length > 64 || domain.length > 254) {
-            return false;
-        }
+    if (options.allowComments && email.includes('(') && email.includes(')')) {
+        email = email.replace(/\(.*?\)/g, '');
+    }
 
-        if (localePart.startsWith('.') || localePart.endsWith('.') || localePart.includes('..')) {
-            return false;
-        }
+    let emailParts = email.split('@');
+    const domain = emailParts.pop();
+    const localPart = emailParts.join('@');
 
-        
-        
-
-
-    } catch (error) {
+    if (options.validateLength && (localPart.length > 64 || domain.length > 254 || email.length > 320)) {
         return false;
     }
+
+    if (!domain.includes('.') || domain.startsWith('.') || domain.endsWith('.')) {
+        return false;
+    }
+
+    if (domain.includes("[") && domain.includes("]")) {
+        return false;
+    }
+
+    if (domain.startsWith('-') || domain.endsWith('-')) {
+        return false;
+    }
+
+    if (options.requireTld && !/\.+[a-zA-Z]/.test(domain)) {
+        return false;
+    }
+
+    let isQuotedString = localPart.startsWith('"') && localPart.endsWith('"');
+    if (!isQuotedString) {
+        if (localPart.startsWith('.') || localPart.endsWith('.') || /\.{2,}/.test(localPart)) {
+            return false;
+        }
+    }
+
+    if (localPart.includes('(') !== localPart.includes(')')) {
+        return false;
+    }
+
+    return true;
 
 }
